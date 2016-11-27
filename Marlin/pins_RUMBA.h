a36d4d13dd3a4414d6e1261c3436fe7e97d1f7f1
Christoph Pech <christoph.pech@gmail.com>
2016-11-27 13:20:38 +0100

    d

--- Marlin/pins_RUMBA.h
+++ Marlin/pins_RUMBA.h
@@ -88,15 +88,19 @@
 // Temperature Sensors
 //
 #if TEMP_SENSOR_0 == -1
-  #define TEMP_0_PIN        6   // Analog Input (connector *K1* on RUMBA thermocouple ADD ON is used)
+  #define TEMP_0_PIN        6   // ANALOG NUMBERING - connector *K1* on RUMBA thermocouple ADD ON is used
+#elif TEMP_SENSOR_0 == 20           // E3D PT100
+  #define TEMP_0_PIN       10   // ANALOG NUMBERING
 #else
-  #define TEMP_0_PIN       15   // Analog Input (default connector for thermistor *T0* on rumba board is used)
+  #define TEMP_0_PIN       15   // ANALOG NUMBERING - default connector for thermistor *T0* on rumba board is used
 #endif
 
 #if TEMP_SENSOR_1 == -1
-  #define TEMP_1_PIN        5   // Analog Input (connector *K2* on RUMBA thermocouple ADD ON is used)
+  #define TEMP_1_PIN        5   // ANALOG NUMBERING - connector *K2* on RUMBA thermocouple ADD ON is used
+#elif TEMP_SENSOR_1 == 20           // E3D PT100
+  #define TEMP_1_PIN        9   // ANALOG NUMBERING
 #else
-  #define TEMP_1_PIN       14   // Analog Input (default connector for thermistor *T1* on rumba board is used)
+  #define TEMP_1_PIN       14   // ANALOG NUMBERING - default connector for thermistor *T1* on rumba board is used
 #endif
 
 #if TEMP_SENSOR_2 == -1
